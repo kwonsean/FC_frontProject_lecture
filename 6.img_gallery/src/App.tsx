78fa1271ac8cdff136ef3ae3f6ded63d8752ef0d
kwonsean <gownn12@naver.com>
2022-06-25 16:53:09 +0900

    style: Set default html & css

--- 6.img_gallery/src/App.tsx
+++ 6.img_gallery/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="addWrapper">
+        <div className="imgAddText">
+          이미지가 없습니다. <br /> 이미지를 추가해주세요.
+        </div>
+        <div className="add-btn">+</div>
+      </div>
     </div>
   );
 }
