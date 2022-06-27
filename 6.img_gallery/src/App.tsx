31be4dd4a015d87aea4dac367a62773cc0655873
kwonsean <gownn12@naver.com>
2022-06-27 23:03:47 +0900

    feat: Install react-dropzone

--- 6.img_gallery/src/App.tsx
+++ 6.img_gallery/src/App.tsx
@@ -1,5 +1,5 @@
-import { read } from "fs";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useRef, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import "./App.css";
 import ImgBox from "./components/ImgBox";
 
@@ -8,6 +8,23 @@ function App() {
 
   const [imgList, setImgList] = useState<string[]>([]);
 
+  const onDrop = useCallback((files: any[]) => {
+    if (!!files) {
+      // !! 이 라이브러리는 여러개의 이미지를 한번에 받을 수 있기 때문에 for문 사용
+      for (let file of files) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onloadend = (e) => {
+          setImgList((cur) => [...cur, e.target?.result as string]);
+        };
+      }
+    }
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  // reac-dropzone으로 대체
   const handleChangeFile = (e: ChangeEvent) => {
     const target = e.target as HTMLInputElement;
     if (!!target.files) {
@@ -20,6 +37,7 @@ function App() {
       };
     }
   };
+
   return (
     <div className="container">
       <div className="imgWrapper">
@@ -33,9 +51,8 @@ function App() {
             이미지가 없습니다. <br /> 이미지를 추가해주세요.
           </div>
         )}
-        <input type="file" ref={inputRef} onChange={(e) => handleChangeFile(e)} />
-        <div className="add-btn" onClick={() => inputRef.current?.click()}>
-          +
+        <div className="add-btn" {...getRootProps()}>
+          <input type="file" ref={inputRef} {...getInputProps()} />+
         </div>
       </div>
     </div>
