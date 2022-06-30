061cd576b8777c2e4b90ee69067946f4836d9aa2
kwonsean <gownn12@naver.com>
2022-06-30 23:40:50 +0900

    feat: Add drawing logic

--- 7.DrawingBoard/README.md
+++ 7.DrawingBoard/README.md
@@ -4,3 +4,8 @@
 
 - `npm install -D parcel @parcel/transformer-sass`
 - 따로 sass를 쓴다고 설정하지 않았는데도 잘 적용이 됨
+
+### HTML요소에 id, class를 모두 주는 이유
+
+- class의 경우 스타일링을 할때 사용하는 용도이고, id는 이벤트를 등록하는 용도
+- 협업을 할때 class 명을 수정하거나 추가, 변경할때 발생할 수 있는 사이드 이펙트를 예방하기 위해
--- 7.DrawingBoard/src/js/main.js
+++ 7.DrawingBoard/src/js/main.js
@@ -1,1 +1,94 @@
-console.log("parcel setting");
+class DrawingBoard {
+  // NONE, BRUSH, ERASER
+  MODE = 'NONE';
+
+  IsMouseDown = false;
+
+  constructor() {
+    this.assginElement();
+    this.initContext();
+    this.addEvent();
+  }
+
+  assginElement() {
+    this.containerEl = document.getElementById('container');
+    this.canvasEl = this.containerEl.querySelector('#canvas');
+    this.toolbarEl = this.containerEl.querySelector('#toolbar');
+    this.brushEl = this.toolbarEl.querySelector('#brush');
+    this.colorPickerEl = this.toolbarEl.querySelector('#colorPicker');
+    this.brushPanelEl = this.containerEl.querySelector('#brushPanel');
+    this.brushSizeInputEl = this.brushPanelEl.querySelector('#brushSize');
+    this.brushSizePreviewEl =
+      this.brushPanelEl.querySelector('#brushSizePreview');
+  }
+
+  initContext() {
+    this.context = this.canvasEl.getContext('2d');
+  }
+
+  addEvent() {
+    this.brushEl.addEventListener('click', this.onClickBrush);
+    this.canvasEl.addEventListener('mousedown', this.onMouseDown);
+    this.canvasEl.addEventListener('mousemove', this.onMouseMove);
+    this.canvasEl.addEventListener('mouseup', this.onMouseUp);
+    this.canvasEl.addEventListener('mouseout', this.onMouseOut);
+    this.brushSizeInputEl.addEventListener('input', this.onChangeBrushSize);
+    this.colorPickerEl.addEventListener('input', this.onChangeColor);
+  }
+
+  onClickBrush = event => {
+    const isActive = event.currentTarget.classList.contains('active');
+    this.MODE = isActive ? 'NONE' : 'BRUSH';
+    this.brushPanelEl.classList.toggle('hide', isActive);
+    // 캔버스의 커서만을 스타일링
+    this.canvasEl.style.cursor = isActive ? 'default' : 'crosshair';
+    this.brushEl.classList.toggle('active');
+  };
+
+  onMouseDown = event => {
+    if (this.MODE === 'NONE') return;
+    this.IsMouseDown = true;
+    const currentPosition = this.getMousePosition(event);
+    this.context.beginPath();
+    this.context.moveTo(currentPosition.x, currentPosition.y);
+    this.context.lineCap = 'round';
+    this.context.strokeStyle = this.colorPickerEl.value;
+    this.context.lineWidth = this.brushSizeInputEl.value;
+  };
+
+  onMouseMove = event => {
+    if (!this.IsMouseDown) return;
+    const currentPosition = this.getMousePosition(event);
+    this.context.lineTo(currentPosition.x, currentPosition.y);
+    this.context.stroke();
+  };
+
+  onMouseUp = () => {
+    if (this.MODE === 'NONE') return;
+    this.IsMouseDown = false;
+  };
+
+  onMouseOut = () => {
+    if (this.MODE === 'NONE') return;
+    this.IsMouseDown = false;
+  };
+
+  getMousePosition = event => {
+    const boundaries = this.canvasEl.getBoundingClientRect();
+    return {
+      x: event.clientX - boundaries.left,
+      y: event.clientY - boundaries.top,
+    };
+  };
+
+  onChangeBrushSize = event => {
+    this.brushSizePreviewEl.style.width = `${event.target.value}px`;
+    this.brushSizePreviewEl.style.height = `${event.target.value}px`;
+  };
+
+  onChangeColor = event => {
+    this.brushSizePreviewEl.style.backgroundColor = event.target.value;
+  };
+}
+
+const drawingBoard = new DrawingBoard();
