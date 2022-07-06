838ddb43b02398f3dbc3ba985dcbc52e886bb8ad
kwonsean <gownn12@naver.com>
2022-07-07 00:04:11 +0900

    refactor: Refactoring code from lecture
    - 강의를 보고 코드 리팩토링

--- 8.TodoList/README.md
+++ 8.TodoList/README.md
@@ -7,3 +7,11 @@
 - `npm i -D rollup-plugin-generate-html-template` index.html을 연결하기 위한 플러그인
 - `npm i -D rollup-plugin-serve rollup-plugin-livereload` dev 서버를 돌리고 watch 옵션을 사용하기 위한 플러그인
 - `rollup-plugin-terser` 코드를 난독화 하는 플러그인
+
+### transitionend
+
+- 이벤트 리스너 중 하나로 트랜지션이 끝날때 작성한 콜백 함수가 실행된다.
+- TODO 요소가 삭제될때 기존 코드는 트랜지션 시간이 1초인 것을 알고 있어서 `setTimeOut`을 이용하여 요소를 DOM에서 지워주었다.  
+  이때 심지어 요소를 지울때 `remove()`를 사용하는 것이 아닌 TODO LIST 요소의 delete 클래스를 가지는 요소를 파악하여 `removeChild()`하는 방식으로 구현함. (remove를 몰랐음)
+- `transitionend`를 이용하면 보다 정확하고 (no hard coding) 간편하게 사용 가능
+- [MDN 공식문서](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/transitionend_event)
--- 8.TodoList/src/js/main.js
+++ 8.TodoList/src/js/main.js
@@ -2,6 +2,8 @@ import '@fortawesome/fontawesome-free/js/all.min';
 import '../scss/style.scss';
 
 class TodoList {
+  // 강의에서는 버튼을 만드는 메서드를 만들어서 id, class 값들을 전달해서 버튼을 하나씩 만드는 방식으로 구현
+  // 근데 결국 이것도 매번 함수가 3번 실행되는 형태이기 때문에 이 방법보다는 제 방법이 더 낫지 않나.. (가독성은 떨어져도)
   todoBtnElements = `<button id="complete-btn" class="complete-btn">
   <i class="fas fa-check"></i>
 </button>
@@ -38,8 +40,8 @@ class TodoList {
   };
 
   onClickTodoEl = event => {
-    const { currentTarget } = event;
-    const btn = event.target.closest('button');
+    const { currentTarget, target } = event;
+    const btn = target.closest('button');
     if (btn === null) return;
     switch (btn.className) {
       case 'delete-btn':
@@ -71,16 +73,12 @@ class TodoList {
     this.todoListEl.append(todoEl);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   deleteTodoList(todoEl) {
     todoEl.classList.add('delete');
-    setTimeout(() => {
-      this.updateTodoList();
-    }, 1000);
-  }
-
-  updateTodoList() {
-    const deletedTodo = this.todoListEl.querySelectorAll('.delete');
-    this.todoListEl.removeChild(deletedTodo[0]);
+    todoEl.addEventListener('transitionend', () => {
+      todoEl.remove();
+    });
   }
 }
 
